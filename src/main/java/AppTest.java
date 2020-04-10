a5d91ee434f8021086aa571cd9765fc339bc1911
Maciej Fanok <mfanok@onet.pl>
2020-04-10 23:16:04 +0200

    Zmiana rozmiaru przycisku.

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -1,15 +1,12 @@
 
 import java.awt.Color;
-import static java.awt.Color.red;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 
+        
 
 public class AppTest extends javax.swing.JFrame {
     
+    AllShapes shapecol = new AllShapes();
 
     public AppTest() {
         initComponents();
@@ -23,10 +20,19 @@ public class AppTest extends javax.swing.JFrame {
         btnBasic = new javax.swing.JButton();
         btnCircle = new javax.swing.JButton();
         btnSquare = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        comboColor = new javax.swing.JComboBox<>();
+        btnRectangle = new javax.swing.JButton();
+        btnTriangle = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        btnBasic.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBasicActionPerformed(evt);
+            }
+        });
+
+        btnCircle.setFont(new java.awt.Font("Dialog", 0, 9)); // NOI18N
         btnCircle.setText("Circle");
         btnCircle.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -34,6 +40,7 @@ public class AppTest extends javax.swing.JFrame {
             }
         });
 
+        btnSquare.setFont(new java.awt.Font("Dialog", 0, 9)); // NOI18N
         btnSquare.setText("Square");
         btnSquare.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -41,10 +48,26 @@ public class AppTest extends javax.swing.JFrame {
             }
         });
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Pink", "Red", "Green", "Yellow", "Brown" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+        comboColor.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "white", "pink", "red", "yellow", "green", "black" }));
+        comboColor.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
+                comboColorActionPerformed(evt);
+            }
+        });
+
+        btnRectangle.setFont(new java.awt.Font("Dialog", 0, 9)); // NOI18N
+        btnRectangle.setText("Rectangle");
+        btnRectangle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRectangleActionPerformed(evt);
+            }
+        });
+
+        btnTriangle.setFont(new java.awt.Font("Dialog", 0, 9)); // NOI18N
+        btnTriangle.setText("Triangle");
+        btnTriangle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTriangleActionPerformed(evt);
             }
         });
 
@@ -55,51 +78,84 @@ public class AppTest extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnCircle)
-                        .addGap(28, 28, 28)
-                        .addComponent(btnSquare)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(156, Short.MAX_VALUE)
-                .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(142, 142, 142))
+                        .addComponent(comboColor, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(btnCircle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnRectangle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 116, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(btnSquare, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(btnTriangle, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCircle)
                     .addComponent(btnSquare))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(66, 66, 66))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnRectangle)
+                    .addComponent(btnTriangle))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(comboColor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCircleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCircleActionPerformed
-	btnBasic.setBorderPainted(false); 
+
+        btnBasic.setBorderPainted(false); 
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
-        btnBasic.setIcon(new ImageIcon(getCircle()));
+        btnBasic.setIcon(new ImageIcon(AllShapes.getCircle()));
+        btnBasic.setBounds(167, 37, 50, 50);
+        btnBasic.repaint();
     }//GEN-LAST:event_btnCircleActionPerformed
 
     private void btnSquareActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSquareActionPerformed
         btnBasic.setBorderPainted(false); 
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
-        btnBasic.setIcon(new ImageIcon(getSquare()));
+        btnBasic.setIcon(new ImageIcon(shapecol.getSquare()));
     }//GEN-LAST:event_btnSquareActionPerformed
 
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+    private void comboColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboColorActionPerformed
+        Color shapeColor = AllShapes.stringToColor(comboColor.getSelectedItem().toString());
+        shapecol.setCColor(shapeColor);
+    }//GEN-LAST:event_comboColorActionPerformed
 
-    }//GEN-LAST:event_jComboBox1ActionPerformed
+    private void btnRectangleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRectangleActionPerformed
+        btnBasic.setBorderPainted(false); 
+	btnBasic.setContentAreaFilled(false); 
+	btnBasic.setOpaque(false);
+        btnBasic.setIcon(new ImageIcon(shapecol.getRectangle()));
+    }//GEN-LAST:event_btnRectangleActionPerformed
+
+    private void btnTriangleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTriangleActionPerformed
+        btnBasic.setBorderPainted(false); 
+	btnBasic.setContentAreaFilled(false); 
+	btnBasic.setOpaque(false);
+        btnBasic.setIcon(new ImageIcon(shapecol.getTriangle()));
+    }//GEN-LAST:event_btnTriangleActionPerformed
+
+    private void btnBasicActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBasicActionPerformed
+        System.out.println("Jestem wciśnięty.");
+    }//GEN-LAST:event_btnBasicActionPerformed
 
 
     public static void main(String args[]) {
@@ -133,40 +189,14 @@ public class AppTest extends javax.swing.JFrame {
             }
         });
     }
-    
-    // Rysowanie figur
-    
-    public static BufferedImage getCircle() {
-        Color CCOLOR = null;
-        
-	BufferedImage circleImage = new BufferedImage(60, 60, BufferedImage.TYPE_INT_ARGB);
 
-	Graphics2D g = (Graphics2D) circleImage.getGraphics();
-	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	g.setColor(CCOLOR);
-	g.fillOval(5, 5, 50, 50);
-	g.drawOval(5, 5, 50, 50);
-	return circleImage;
-	    
-    }
-    
-    public static BufferedImage getSquare() {
-
-	BufferedImage squareImage = new BufferedImage(60, 60, BufferedImage.TYPE_INT_ARGB);
-
-	Graphics2D g = (Graphics2D) squareImage.getGraphics();
-	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-	g.setColor(Color.PINK);
-	g.fill(new Rectangle(5, 5, 50, 50));
-	g.draw(new Rectangle(5, 5, 50, 50));
-	return squareImage;
-	    
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBasic;
     private javax.swing.JButton btnCircle;
+    private javax.swing.JButton btnRectangle;
     private javax.swing.JButton btnSquare;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JButton btnTriangle;
+    private javax.swing.JComboBox<String> comboColor;
     // End of variables declaration//GEN-END:variables
 }
