b2b48ec00e98cfc70dc793a51577e44c7dce9272
Dawid Zając <kicusd@gmail.com>
2020-04-22 13:47:02 +0200

    aktualizacja ramki

--- src/main/java/AppTest.java
+++ src/main/java/AppTest.java
@@ -1,5 +1,6 @@
 
 import java.awt.Color;
+import java.awt.Label;
 import javax.swing.ImageIcon;
 
         
@@ -10,27 +11,38 @@ public class AppTest extends javax.swing.JFrame {
 
     public AppTest() {
         initComponents();
+        Label.setBounds(70, 80, 100, 100);
+        Label.repaint();
     }
+    
+    
 
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnBasic = new javax.swing.JButton();
+        jLayeredPane1 = new javax.swing.JLayeredPane();
         btnCircle = new javax.swing.JButton();
         btnSquare = new javax.swing.JButton();
         comboColor = new javax.swing.JComboBox<>();
         btnRectangle = new javax.swing.JButton();
         btnTriangle = new javax.swing.JButton();
+        btnBasic = new javax.swing.JButton();
+        Label = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jLayeredPane1Layout = new javax.swing.GroupLayout(jLayeredPane1);
+        jLayeredPane1.setLayout(jLayeredPane1Layout);
+        jLayeredPane1Layout.setHorizontalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jLayeredPane1Layout.setVerticalGroup(
+            jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        btnBasic.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBasicActionPerformed(evt);
-            }
-        });
 
         btnCircle.setFont(new java.awt.Font("Dialog", 0, 9)); // NOI18N
         btnCircle.setText("Circle");
@@ -71,47 +83,60 @@ public class AppTest extends javax.swing.JFrame {
             }
         });
 
+        btnBasic.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBasicActionPerformed(evt);
+            }
+        });
+
+        Label.setAlignmentX(10.0F);
+        Label.setAlignmentY(70.0F);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(55, Short.MAX_VALUE)
+                .addComponent(comboColor, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31))
             .addGroup(layout.createSequentialGroup()
-                .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(comboColor, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(btnCircle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnRectangle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(20, 20, 20)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnBasic, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 116, Short.MAX_VALUE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(btnCircle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(btnRectangle, javax.swing.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE))
+                                .addGap(27, 27, 27)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(btnSquare, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(btnTriangle, javax.swing.GroupLayout.DEFAULT_SIZE, 72, Short.MAX_VALUE))
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))))
+                                    .addComponent(btnTriangle, javax.swing.GroupLayout.DEFAULT_SIZE, 89, Short.MAX_VALUE)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(106, 106, 106)
+                        .addComponent(Label, javax.swing.GroupLayout.PREFERRED_SIZE, 9, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Label, javax.swing.GroupLayout.DEFAULT_SIZE, 11, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnBasic, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCircle)
                     .addComponent(btnSquare))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnRectangle)
                     .addComponent(btnTriangle))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(comboColor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
+                .addGap(35, 35, 35))
         );
 
         pack();
@@ -123,8 +148,10 @@ public class AppTest extends javax.swing.JFrame {
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
         btnBasic.setIcon(new ImageIcon(AllShapes.getCircle()));
-        btnBasic.setBounds(167, 37, 50, 50);
         btnBasic.repaint();
+        Label.setIcon(new ImageIcon(AllShapes.getCircleB()));
+        Label.setBounds(70, 80, 100, 100);
+        //Label.repaint();
     }//GEN-LAST:event_btnCircleActionPerformed
 
     private void btnSquareActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSquareActionPerformed
@@ -132,6 +159,9 @@ public class AppTest extends javax.swing.JFrame {
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
         btnBasic.setIcon(new ImageIcon(shapecol.getSquare()));
+        Label.setIcon(new ImageIcon(shapecol.getCircleB()));
+        Label.setBounds(50, 10, 100, 100);
+        Label.repaint();
     }//GEN-LAST:event_btnSquareActionPerformed
 
     private void comboColorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboColorActionPerformed
@@ -144,6 +174,9 @@ public class AppTest extends javax.swing.JFrame {
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
         btnBasic.setIcon(new ImageIcon(shapecol.getRectangle()));
+        Label.setIcon(new ImageIcon(shapecol.getCircleB()));
+        Label.setBounds(50, 10, 100, 100);
+        Label.repaint();
     }//GEN-LAST:event_btnRectangleActionPerformed
 
     private void btnTriangleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTriangleActionPerformed
@@ -151,6 +184,9 @@ public class AppTest extends javax.swing.JFrame {
 	btnBasic.setContentAreaFilled(false); 
 	btnBasic.setOpaque(false);
         btnBasic.setIcon(new ImageIcon(shapecol.getTriangle()));
+        Label.setIcon(new ImageIcon(shapecol.getTriangleB()));
+        Label.setBounds(50, 10, 100, 100);
+        Label.repaint();
     }//GEN-LAST:event_btnTriangleActionPerformed
 
     private void btnBasicActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBasicActionPerformed
@@ -192,11 +228,13 @@ public class AppTest extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Label;
     private javax.swing.JButton btnBasic;
     private javax.swing.JButton btnCircle;
     private javax.swing.JButton btnRectangle;
     private javax.swing.JButton btnSquare;
     private javax.swing.JButton btnTriangle;
     private javax.swing.JComboBox<String> comboColor;
+    private javax.swing.JLayeredPane jLayeredPane1;
     // End of variables declaration//GEN-END:variables
 }
